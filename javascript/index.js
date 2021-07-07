0fd3b76e26860665e583ec881ee618dbdbc13ee0
“Pablo <pablorios1603@gmail.com>
2021-07-07 12:53:50 -0500

    Se hizo uso de herencia en js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,8 @@
 var car = new Car("ASDD", new Account("El nombre","ASF"))
 car.passenger = 4
 car.printDataCar();
+
+
+var uberX = new UberX("DFDFG", new Account("Otro nombre","SDF34"),"Chevrolet","Spark")
+uberX.passenger = 4
+uberX.printDataCar();
